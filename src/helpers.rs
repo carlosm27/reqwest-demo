d198fe297ebd43f8011a6cc1bb0c232bc796981a
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-10-15 21:07:41 -0400

    Update helpers.rs

--- src/helpers.rs
+++ src/helpers.rs
@@ -4,7 +4,7 @@ use std::io::BufRead;
 use std::io;
 
 
-pub fn read_file_lines_to_vec(filename: &str) -> io::Result<Vec<String>> { 
+pub fn _read_file_lines_to_vec(filename: &str) -> io::Result<Vec<String>> { 
     let file_in = fs::File::open(filename)?; 
     let file_reader = BufReader::new(file_in); 
     Ok(file_reader.lines().filter_map(io::Result::ok).collect()) 
